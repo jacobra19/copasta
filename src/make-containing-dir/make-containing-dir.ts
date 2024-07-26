09ed5187a00d8e1a4f3036bab283fdbc28a63e5f
Yakov Rakhamimov <yakovr@wix.com>
2024-07-26 22:00:03 +0300

    refactor: Update make-containing-dir.ts to use cwd() instead of execPath

--- src/make-containing-dir/make-containing-dir.ts
+++ src/make-containing-dir/make-containing-dir.ts
@@ -1,11 +1,10 @@
 import { mkdir } from "fs/promises"
 import { ErrorsMessages } from "../errors-messages"
-import { execPath } from "process"
+import { cwd } from "process"
 
 export const makeContainingDir = async (dirName: string): Promise<string | void> => {
-  const dirPath = `${execPath}/${dirName}`
+  const dirPath = `${cwd()}/${dirName}`
   console.log('dirPath', dirPath)
-  console.log('execPath', execPath)
   try {
     await mkdir(dirPath)
     return dirPath
