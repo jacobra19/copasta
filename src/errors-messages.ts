d6f592e38560ff3b0ba4324cb0b30100ba4bc82c
Yakov Rakhamimov <yakovr@wix.com>
2024-07-26 20:08:04 +0300

    feat: Add error messages enum for better error handling

---
+++ src/errors-messages.ts
@@ -0,0 +1,4 @@
+export enum ErrorsMessages {
+  MissingComponentName = 'Missing component name, please provide a component name \n e.g. `npx copasta my-component`',
+  MakingDirError = 'Error creating directory',
+}
