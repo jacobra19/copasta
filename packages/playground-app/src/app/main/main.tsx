aa68895d8c39ae0d20963964b728ff72ed0a33ef
Yakov Rakhamimov <jacobra19@gmail.com>
2024-08-30 19:31:49 +0300

    chore: Update npm dependencies for copasta playground app

---
+++ packages/playground-app/src/app/main/main.tsx
@@ -0,0 +1,93 @@
+import {
+  AppShell,
+  Button,
+  Code,
+  Flex,
+  Input,
+  JsonInput,
+  Select,
+  TagsInput,
+  Text,
+} from '@mantine/core';
+import Editor from '@monaco-editor/react';
+import { TemplateSettingsToolbar } from './template-settings-toolbar/template-settings-toolbar';
+import { usePlaygroundContext } from './playground.context';
+
+export const Main = () => {
+  const { editorLanguage, moduleName } = usePlaygroundContext();
+
+  return (
+    <AppShell.Main>
+      <Flex gap={'lg'}>
+        <Flex
+          flex={1}
+          direction={'column'}
+          style={{ border: '1px solid grey' }}
+        >
+          <TemplateSettingsToolbar />
+          <pre>{JSON.stringify(editorLanguage, null, 2)}</pre>
+          <pre>{JSON.stringify(moduleName, null, 2)}</pre>
+          {/* <Flex>
+        <Flex flex={1} direction={'column'}>
+          <Editor
+            // onChange={}
+            height={'calc(100vh - 110px)'}
+            defaultLanguage={editorLanguage?.id}
+            language={editorLanguage?.id}
+            theme='vs-dark'
+            width={'100%'}
+            value={editorValue}
+            onChange={(value) => {
+              console.log('Editor onChange value', value);
+
+              setEditorValue(value ?? '');
+            }}
+            wrapperProps={{}}
+          />
+        </Flex>
+        <Flex flex={1} direction={'column'}>
+          <Flex direction={'column'}>
+            <Code>
+              <Text>copasta.config.json</Text>
+            </Code>
+            <Editor
+              // onChange={}
+              height={'calc(100vh - 110px)'}
+              defaultLanguage={'json'}
+              theme='vs-dark'
+              width={'100%'}
+              value={JSON.stringify(
+                {
+                  templates: [
+                    {
+                      extension: editorLanguage.id,
+                      content: editorValue,
+                    },
+                  ],
+                },
+                null,
+                2
+              )}
+              onChange={(value) => {
+                console.log('Editor onChange value', value);
+
+                setEditorValue(value ?? '');
+              }}
+            />
+          </Flex>
+          <Code>
+            <Text>{JSON.stringify(editorValue)}</Text>
+          </Code>
+        </Flex>
+      </Flex> */}
+          <Flex align={'flex-end'}>
+            <Button>Add Template</Button>
+          </Flex>
+        </Flex>
+        <Flex flex={1} style={{ border: '1px solid grey' }}>
+          preview
+        </Flex>
+      </Flex>
+    </AppShell.Main>
+  );
+};
---
+++ packages/playground-app/src/app/main/template-settings-toolbar/template-settings-toolbar.tsx
@@ -0,0 +1,66 @@
+import { Flex, Input, Select } from '@mantine/core';
+import { usePlaygroundContext } from '../playground.context';
+import { useMemo } from 'react';
+
+export const TemplateSettingsToolbar = () => {
+  const {
+    editorLanguage,
+    handleEditorLanguageIdChange,
+    handleModuleNameChange,
+    handleSelectedExtensionChange,
+    selectedExtension,
+    moduleName,
+    languages,
+  } = usePlaygroundContext();
+
+  const languagesData = useMemo(
+    () =>
+      languages.map((lang) => ({
+        value: lang.id,
+        label: lang.id,
+      })),
+    [languages]
+  );
+
+  const extensionsData = useMemo(
+    () =>
+      editorLanguage.extensions!.map((ext) => ({
+        value: ext,
+        label: ext,
+      })),
+    [editorLanguage.extensions]
+  );
+
+  console.log('TemplateSettingsToolbar editorLanguage', editorLanguage);
+  return (
+    <Flex gap={'md'}>
+      <Select
+        value={editorLanguage.id}
+        label='Template Language'
+        onChange={(value) => {
+          if (value) handleEditorLanguageIdChange(value);
+        }}
+        allowDeselect={false}
+        data={languagesData}
+      />
+      <Select
+        label='File Extension'
+        value={selectedExtension}
+        onChange={(value) => {
+          if (value) handleSelectedExtensionChange(value);
+        }}
+        allowDeselect={false}
+        data={extensionsData}
+      />
+      <Input.Wrapper label='Module name'>
+        <Input
+          placeholder='Enter Module Name'
+          value={moduleName}
+          onChange={(e) => {
+            handleModuleNameChange(e.currentTarget.value);
+          }}
+        />
+      </Input.Wrapper>
+    </Flex>
+  );
+};
